dbdcc4dd42e4cfa5695f2bece949d38911d233c9
Lucas Ribeiro <lucas.daniel@gmail.com>
2020-04-03 12:09:51 -0400

    add all components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,90 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>My Shop</h1>
-    <p class="animated fadeInRight">Take a look at our offerings below</p>
-    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <navbar :cart="cart" :cartQty="cartQty" :cartTotal="cartTotal" @toggle="toggleSliderStatus" @delete="deleteItem"></navbar>
+    <price-slider :sliderStatus="sliderStatus" :maximum.sync="maximum"></price-slider>
+    <product-list :products="products" :maximum="maximum" @add="addItem"></product-list>
   </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+//when we have more than one component inside file we need specify one using {}
+import ProductList from "./components/ProductList.vue";
+import PriceSlider from "./components/PriceSlider.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
   name: "App",
   components: {
-    FontAwesomeIcon
+    ProductList,
+    PriceSlider,
+    Navbar
+  },
+  data: function() {
+    return {
+      maximum: 99,
+      products: null,
+      cart: [],
+      sliderStatus: true
+    };
+  },
+  methods: {
+    toggleSliderStatus: function() {
+      this.sliderStatus = !this.sliderStatus;
+    },
+    addItem: function(product) {
+      var whichProduct;
+      var existing = this.cart.filter(function(item, index) {
+        if (item.product.id == Number(product.id)) {
+          whichProduct = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      if (existing.length) {
+        this.cart[whichProduct].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
+    },
+    deleteItem: function(id) {
+      if (this.cart[id].qty > 1) {
+        this.cart[id].qty--;
+      } else {
+        this.cart.splice(id, 1);
+      }
+    }
+  },
+  computed: {
+    cartTotal: function() {
+      let sum = 0;
+      for (let key in this.cart) {
+        sum = sum + this.cart[key].product.price * this.cart[key].qty;
+      }
+      return sum;
+    },
+    cartQty: function() {
+      let qty = 0;
+      for (let key in this.cart) {
+        qty = qty + this.cart[key].qty;
+      }
+      return qty;
+    }
+  },
+  mounted: function() {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then(response => response.json())
+      .then(data => {
+        this.products = data;
+      });
   }
 };
 </script>
+
+<style>
+body {
+  overflow: hidden;
+}
+</style>
